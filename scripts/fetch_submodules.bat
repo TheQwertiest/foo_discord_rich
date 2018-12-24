9822468b156ee7e38d5bd879c44c688223826e59
TheQwertiest <qwertiest@mail.ru>
2018-12-24 14:57:11 +0300

    [scripts] Updated submodule fetch script
    - Fixed fetching of older commits.

--- scripts/fetch_submodules.bat
+++ scripts/fetch_submodules.bat
@@ -14,16 +14,33 @@ git submodule sync
 if errorlevel 1 goto fail
 git submodule foreach git reset --hard
 if errorlevel 1 goto fail
-git submodule update --init --depth=10
+for /d %%i in ("%ROOT_DIR%submodules\*") do (
+  call :FetchSubmodule submodules\%%~nxi
+)
+exit /b 0
+
+:FetchSubmodule
+set SUBMODULE_REL_PATH=%1
+echo Fetching %SUBMODULE_REL_PATH%...
+git submodule update --init --depth=10 -- %SUBMODULE_REL_PATH%
 if errorlevel 1 (
-  git submodule update --init --depth=50 --force
+  git submodule update --init --depth=50 --force -- %SUBMODULE_REL_PATH%
   if errorlevel 1 (
-    git submodule update --init --force
+    cd %SUBMODULE_REL_PATH%
+    if errorlevel 1 goto fail
+    rem Shallow copy does not honour default branch config
+    git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+    if errorlevel 1 goto fail
+    cd %ROOT_DIR%
+    if errorlevel 1 goto fail
+    git submodule deinit --force -- %SUBMODULE_REL_PATH%
+    if errorlevel 1 goto fail
+    git submodule update --init --force -- %SUBMODULE_REL_PATH%
     rem We tried T_T
     if errorlevel 1 goto fail
   )
 )
-exit /b 0
+goto :eof
 
 :fail
 echo Failed!
