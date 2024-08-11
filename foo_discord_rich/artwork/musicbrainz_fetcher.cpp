ced5ffad51da61b84348d6618cfeeacb32c66a6f
TheQwertiest <theqwertiest@outlook.com>
2024-08-11 16:56:49 +0200

    Verify user MBID and skip if needed; fixed album/artist arg positions (#63)

--- foo_discord_rich/artwork/musicbrainz_fetcher.cpp
+++ foo_discord_rich/artwork/musicbrainz_fetcher.cpp
@@ -8,9 +8,36 @@
 namespace
 {
 
+using namespace drp;
+
+bool IsValidGuid( const qwr::u8string& str )
+{
+    GUID guid;
+    HRESULT hr = IIDFromString( qwr::unicode::ToWide( fmt::format( "{{{}}}", str ) ).c_str(), &guid );
+    return SUCCEEDED( hr );
+}
+
+void LogRequest( const cpr::Response& resp )
+{
+    if ( config::advanced::logWebRequests )
+    {
+        LogDebug( "Request: {}", resp.url.str() );
+    }
+    if ( config::advanced::logWebResponses )
+    {
+        LogDebug(
+            "Response:\n"
+            "  Code: {}\n"
+            "  Body:\n"
+            "{}\n",
+            resp.status_code,
+            resp.text );
+    }
+}
+
 /// @throw qwr::QwrException
 /// @throw exception_aborted
-std::optional<qwr::u8string> FetchReleaseMbid( const qwr::u8string& album, const qwr::u8string& artist, abort_callback& aborter )
+std::optional<qwr::u8string> FetchReleaseMbid( const qwr::u8string& artist, const qwr::u8string& album, abort_callback& aborter )
 {
     using json = nlohmann::json;
 
@@ -20,6 +47,7 @@ std::optional<qwr::u8string> FetchReleaseMbid( const qwr::u8string& album, const
             { "query", fmt::format( "artist:\"{}\"+releasegroup:\"{}\"", artist, album ) },
             { "fmt", "json" },
         } );
+    LogRequest( releaseGroupResp );
     if ( releaseGroupResp.status_code == 404 )
     {
         return std::nullopt;
@@ -46,6 +74,7 @@ std::optional<qwr::u8string> FetchReleaseMbid( const qwr::u8string& album, const
             auto releaseResp = cpr::Get(
                 cpr::Url{ fmt::format( "https://www.musicbrainz.org/ws/2/release/{}", releaseId ) },
                 cpr::Header{ { "Accept", "application/json" } } );
+            LogRequest( releaseResp );
             if ( releaseResp.status_code != 200 )
             {
                 throw qwr::QwrException( "Failed to fetch MB release\nCode: {}\nError: {}", releaseResp.status_code, releaseResp.reason );
@@ -73,6 +102,7 @@ std::optional<qwr::u8string> FetchAlbumArtUrl( const qwr::u8string& mbid )
 {
     auto resp = cpr::Get(
         cpr::Url{ fmt::format( "http://coverartarchive.org/release/{}/front-1200", mbid ) } );
+    LogRequest( resp );
     if ( resp.status_code == 404 )
     {
         return std::nullopt;
@@ -89,19 +119,26 @@ std::optional<qwr::u8string> FetchAlbumArtUrl( const qwr::u8string& mbid )
 namespace drp::musicbrainz
 {
 
-std::optional<qwr::u8string> FetchArt( const qwr::u8string& album, const qwr::u8string& artist, const std::optional<qwr::u8string>& userReleaseMbidOpt )
+std::optional<qwr::u8string> FetchArt( const qwr::u8string& artist, const qwr::u8string& album, const std::optional<qwr::u8string>& userReleaseMbidOpt )
 {
     auto& aborter = qwr::GlobalAbortCallback::GetInstance();
 
     if ( userReleaseMbidOpt )
     {
-        const auto urlOpt = FetchAlbumArtUrl( *userReleaseMbidOpt );
-        return urlOpt;
+        if ( !IsValidGuid( *userReleaseMbidOpt ) )
+        {
+            LogWarning( fmt::format( "Invalid MBID detected: `{}`. Skipping...", *userReleaseMbidOpt ) );
+        }
+        else
+        {
+            const auto urlOpt = FetchAlbumArtUrl( *userReleaseMbidOpt );
+            return urlOpt;
+        }
     }
 
     aborter.check();
 
-    const auto releaseMbidOpt = FetchReleaseMbid( album, artist, aborter );
+    const auto releaseMbidOpt = FetchReleaseMbid( artist, album, aborter );
     if ( !releaseMbidOpt )
     {
         return std::nullopt;
